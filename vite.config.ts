26f0f680a7fe06b5fe97bdb69b8278c6e33759ec
soikat-san <soikat.codes@gmail.com>
2026-04-10 23:23:14 +0530

    vitest coverage

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,16 @@ import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   test: {
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/test/setup.ts",
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "html"],
+      exclude: ["src/main.tsx", "src/router.tsx", "src/api/**"],
+    },
   },
 });
